58e9da4c5f967b88faddd616358f712a019d7581
theroasters84-wq <theroasters84@gmail.com>
2026-02-01 00:48:09 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,54 +1,27 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBDOAlwLn4P5PMlwkg_Hms6-4f9fEcBKn8",
-    authDomain: "bellgo-5dbe5.firebaseapp.com",
-    projectId: "bellgo-5dbe5",
-    storageBucket: "bellgo-5dbe5.firebasestorage.app",
-    messagingSenderId: "799314495253",
-    appId: "1:799314495253:web:baf6852f2a065c3a2e8b1c",
-    measurementId: "G-379ETZJP8H"
-};
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-// BACKGROUND HANDLER
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[SW] Background message: ', payload);
-  
-  const notificationTitle = '🚨 ΚΛΗΣΗ ΚΟΥΖΙΝΑΣ!';
-  const notificationOptions = {
-    body: 'Πάτα ΕΔΩ για προβολή.',
-    icon: 'https://cdn-icons-png.flaticon.com/512/10337/10337229.png',
-    tag: 'alarm-notification',
-    renotify: true,
-    requireInteraction: true, // Να μένει εκεί μέχρι να το πατήσεις
-    data: { url: '/' }
-  };
-
-  return self.registration.showNotification(notificationTitle, notificationOptions);
+firebase.initializeApp({
+ apiKey:"AIzaSyDyqu5tLS2hY3kS1XvHOrfw8CatB_mwI9A",
+ authDomain:"bellgofinall.firebaseapp.com",
+ projectId:"bellgofinall",
+ messagingSenderId:"654570848224",
+ appId:"1:654570848224:web:50704c332d885e3a0df585"
 });
 
-// 🔥 CLICK HANDLER (FOCUS TAB) 🔥
-self.addEventListener('notificationclick', function(event) {
-  console.log('Notification click received.');
-  event.notification.close();
+const messaging=firebase.messaging();
 
-  event.waitUntil(
-    clients.matchAll({type: 'window', includeUncontrolled: true}).then(function(clientList) {
-      // 1. Ψάχνουμε αν υπάρχει ήδη ανοιχτή καρτέλα
-      for (var i = 0; i < clientList.length; i++) {
-        var client = clientList[i];
-        if (client.url.indexOf('/') > -1 && 'focus' in client) {
-          return client.focus(); // Την φέρνουμε μπροστά
-        }
-      }
-      // 2. Αν δεν υπάρχει, ανοίγουμε νέα
-      if (clients.openWindow) {
-        return clients.openWindow('/');
-      }
-    })
-  );
+messaging.setBackgroundMessageHandler(()=> {
+ return self.registration.showNotification("🚨 ΚΛΗΣΗ ΚΟΥΖΙΝΑΣ",{
+  body:"Πάτα για άμεση αποδοχή",
+  icon:"https://cdn-icons-png.flaticon.com/512/10337/10337229.png",
+  requireInteraction:true,
+  vibrate:[500,500,500],
+  data:{url:'/'}
+ });
+});
+
+self.addEventListener('notificationclick',e=>{
+ e.notification.close();
+ e.waitUntil(clients.openWindow('/'));
 });
