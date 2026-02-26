c17cd617338a53e197dd26eb94951574ceedfb8b
Giorgos <theroasters84@gmail.com>
2026-02-26 15:34:02 +0200

    a

--- public/sundromes.js
+++ public/sundromes.js
@@ -22,7 +22,7 @@ export const Sundromes = {
             price: 15, 
             year: 1994,
             desc: 'QR για παραγγελίες delivery και διαχείριση κρατήσεων.',
-            ui_ids: ['resWrapper', 'btnShowLink', 'btnSettings', 'btnFakeLock', 'btnSettingsQr', 'btnSettingsGeneral', 'btnMenuToggle', 'btnSettingsStore', 'rowSwitchCust', 'rowStaffCharge', 'secSchedule'] // ✅ Customers & Staff Charge here
+            ui_ids: ['resWrapper', 'btnShowLink', 'btnSettings', 'btnFakeLock', 'btnSettingsQr', 'btnSettingsGeneral', 'btnMenuToggle', 'btnSettingsStore', 'rowSwitchCust', 'secSchedule'] // ✅ Only Customers switch here
         },
         { 
             key: 'pack_tables', 
